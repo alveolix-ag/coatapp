2709e6f3dc3a77fac3db751d97d68baf6b7debf4
danielnzg85 <daniel.nakhaee-zadeh.gutierrez@epfl.ch>
2019-07-09 12:16:26 +0200

    Update socket_client.py

--- protocols/socket_client.py
+++ protocols/socket_client.py
@@ -2,7 +2,7 @@
 import socket
 
 HOST = '169.254.87.247'    # The remote host
-PORT = 50007            # The same port as used by the server
+PORT = 50005            # The same port as used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	s.connect((HOST, PORT))
 	rotate_state = input("rotate state(1)apical or basal(2)")
