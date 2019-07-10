ff2c041af42d76a6c7fd700a46ff96038de2303a
danielnzg85 <daniel.nakhaee-zadeh.gutierrez@epfl.ch>
2019-07-10 09:52:51 +0200

    update usetip function

--- protocols/calibrate.py
+++ protocols/calibrate.py
@@ -28,19 +28,24 @@ if 'pipette_300' not in locals():
     pipette_300 = instruments.P300_Single(mount='left', tip_racks=[tiprack])
 
 piwells =["A1","B1","C1","D1","E1","F1","G1","H1","A2","B2","C2","D2","E2","F2","G2","H2","A3","B3","C3","D3","E3","F3","G3","H3","A4","B4","C4","D4","E4","F4","G4","H4","A5","B5","C5","D5","E5","F5","G5","H5","A6","B6","C6","D6","E6","F6","G6","H6","A7","B7","C7","D7","E7","F7","G7","H7","A8","B8","C8","D8","E8","F8","G8","H8","A9","B9","C9","D9","E9","F9","G9","H9","A10","B10","C10","D10","E10","F10","G10","H10","A11","B11","C11","D11","E11","F11","G11","H11","A12","B12","C12","D12","E12","F12","G12","H12"]
-def usetip(val = 3,rst = 0):
+def usetip(val = 3,rst = 1):
+    piwells =["A1","B1","C1","D1","E1","F1","G1","H1","A2","B2","C2","D2","E2","F2","G2","H2","A3","B3","C3","D3","E3","F3","G3","H3","A4","B4","C4","D4","E4","F4","G4","H4","A5","B5","C5","D5","E5","F5","G5","H5","A6","B6","C6","D6","E6","F6","G6","H6","A7","B7","C7","D7","E7","F7","G7","H7","A8","B8","C8","D8","E8","F8","G8","H8","A9","B9","C9","D9","E9","F9","G9","H9","A10","B10","C10","D10","E10","F10","G10","H10","A11","B11","C11","D11","E11","F11","G11","H11","A12","B12","C12","D12","E12","F12","G12","H12"]
     if val == 0:
         varwells = rst;
+        c_well = varwells;
     elif val == 1:
-        with open('tipwells', 'rb') as f:
+        with open('tipw', 'rb') as f:
             varwells = pickle.load(f);
+            c_well = varwells;
+        return piwells[c_well]
     else:
-        with open('tipwells', 'rb') as f:
+        with open('tipw', 'rb') as f:
             varwells = pickle.load(f);
+        c_well = varwells;
         varwells = varwells+1;
-    with open('tipwells', 'wb') as f:
-        pickle.dump(varwells, f);
-    return varwells
+    with open('tipw', 'wb') as f:
+        pickle.dump(varwells, f);    
+    return c_well
 
 
 with open('z_calibration', 'rb') as f:
