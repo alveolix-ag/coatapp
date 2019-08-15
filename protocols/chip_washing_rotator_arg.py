16e9538c49ce1a9d23e052c0109d04f7fd2d600e
danielnzg85 <danielnzg85@gmail.com>
2019-08-15 17:16:27 +0200

    Update chip_washing_rotator_arg.py

--- protocols/chip_washing_rotator_arg.py
+++ protocols/chip_washing_rotator_arg.py
@@ -145,15 +145,15 @@ def aspirate_vol_wash(vol_array, in_well = 0): #this function controls the volum
     else:
         return 0, 0
 
-def aspirate_from_well(well, z_distance, volume, z_change = 0):
+def aspirate_from_well(well, volume, z_change = 0):
     positions=[[0.5,"x"],[-0.5,"x"],[0.5,"y"],[-0.5,"y"]];
     volume=int(volume/5)
     for it in range (5):
         if it == 0:
-            pipette_300.aspirate(volume, ax_6.wells(well).top(z_distance+z_change))
-            pipette_300.delay(seconds=1)
+            pipette_300.aspirate(volume, ax_6.wells(well).top(z_change))
+            pipette_300.delay(seconds=4)
         else:
-            pipette_300.move_to(ax_6.wells(well).top(2))
+            pipette_300.move_to(ax_6.wells(well).top(z_change))
             calibration_functions.jog_instrument(instrument=pipette_300,distance=positions[it-1][0],axis=positions[it-1][1],robot=robot)
             pipette_300.aspirate(vol_asp[i])
             pipette_300.delay(seconds=1)
@@ -184,7 +184,7 @@ if advance_mode == 1:
     pipette_300.pick_up_tip(tiprack.wells(piwells[int(usetip())]))
     for i in range(len(wells_to_coat)):
         for u in range(num_asp[i]):
-            aspirate_from_well(wells_to_coat[i], z_distance, vol_asp[i], z_list[i])
+            aspirate_from_well(wells_to_coat[i], vol_asp[i], z_list[i])
             #pipette_300.aspirate(vol_asp[i], ax_6.wells(wells_to_coat[i]).top(z_distance+z_list[i]))
             pipette_300.delay(seconds=2)
             vol_in = vol_asp_mat[d] + vol_in;
