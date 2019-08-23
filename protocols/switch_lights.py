0fbf4555bc5ea09fc6605b3482a4dd113f38e967
danielnzg85 <danielnzg85@gmail.com>
2019-08-23 17:08:55 +0200

    Update switch_lights.py

--- protocols/switch_lights.py
+++ protocols/switch_lights.py
@@ -3,14 +3,14 @@ import argparse
 
 
 parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('-o', '--on', type=bool)
+parser.add_argument('-o', '--on', type=str)
 args = parser.parse_args() #this is the variable that stores the inputs from the UI
-state = args.on
+state = str(args.on)
 
-if state == True:
+if state == "ON":
 	robot._driver.turn_on_rail_lights()
 	print("ON")
 	
-elif state == False:
+elif state == "OFF":
 	robot._driver.turn_off_rail_lights()
 	print("OFF")
