532ee31245474ea1f1959a8ace1fd9bb2e3f67f6
danielnzg85 <danielnzg85@gmail.com>
2019-08-28 14:12:19 +0200

    Update client_socket_test1.py

--- protocols/client_socket_test1.py
+++ protocols/client_socket_test1.py
@@ -7,30 +7,30 @@ with open('host_ip', 'rb') as f:
 HOST = cu_ip    # The remote host
 PORT = 50005   # The same port as used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        rotate_st = str.encode("Tip")
-        s.send(rotate_st)
-        #Data is received when the chip is correctly flipped.
-        data = s.recv(1024)
-        print('Received', repr(data))
+    s.connect((HOST, PORT))
+    rotate_st = str.encode("Tip")
+    s.send(rotate_st)
+    #Data is received when the chip is correctly flipped.
+    data = s.recv(1024)
+    print('Received', repr(data))
 
-      	hello = input("hello")
+hello = input("hello")
 
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        state = str.encode("Rotate")
-        s.send(state)
-        data = s.recv(1024)
-        print('Received', repr(data))
-
-        hello = input("hello")	
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        state1 = str.encode("finished")
-        s.send(state1)
-        data = s.recv(1024)
-        print('Received', repr(data))
+    s.connect((HOST, PORT))
+    state = str.encode("Rotate")
+    s.send(state)
+    data = s.recv(1024)
+    print('Received', repr(data))
 
-        hello = input("hello")
+hello = input("hello")	
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    state1 = str.encode("finished")
+    s.send(state1)
+    data = s.recv(1024)
+    print('Received', repr(data))
+
+hello = input("hello")
